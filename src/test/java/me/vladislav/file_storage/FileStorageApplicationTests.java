ff080aaa77f8f66e6f3a7533bbeada7f31c70fb3
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-17 20:32:44 +0300

    Initial commit

---
+++ src/test/java/me/vladislav/file_storage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vladislav.file_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
