06191eb4d08d1dd03911f1d86fa7a36d1ebb4306
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-26 00:04:10 +0300

    feat: added SpringSecurityConfiguration.java;

---
+++ src/main/java/me/vladislav/file_storage/configurations/SpringSecurityConfiguration.java
@@ -0,0 +1,10 @@
+package me.vladislav.file_storage.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration {
+
+}
