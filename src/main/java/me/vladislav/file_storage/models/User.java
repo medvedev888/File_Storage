9b06229565eecc1ec9e8e2a6b4c9cbba0d6f216e
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-25 23:58:04 +0300

    feat: added User model;
    added User.java;

---
+++ src/main/java/me/vladislav/file_storage/models/User.java
@@ -0,0 +1,30 @@
+package me.vladislav.file_storage.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "users_login", columnList = "login", unique = true)
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "login", unique = true, nullable = false)
+    private String login;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
