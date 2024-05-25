3e350eb5c344dc72f7ce1e107d64481aa500ff25
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-25 23:58:26 +0300

    feat: added UserAlreadyExistException.java;

---
+++ src/main/java/me/vladislav/file_storage/UserAlreadyExistException.java
@@ -0,0 +1,11 @@
+package me.vladislav.file_storage;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
