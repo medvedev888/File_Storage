f75c3859ad8d878313c88acad2dc8bee005209ed
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-26 00:03:06 +0300

    refactor: added post method for registering users and a service for working with user;
    added UserService.java(improvements needed);
    updated AuthenticationController.java (improvements needed);

--- src/main/java/me/vladislav/file_storage/controllers/AuthenticationController.java
+++ src/main/java/me/vladislav/file_storage/controllers/AuthenticationController.java
@@ -1,18 +1,44 @@
 package me.vladislav.file_storage.controllers;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.vladislav.file_storage.dto.UserDTO;
+import me.vladislav.file_storage.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final UserService userService;
+
     @GetMapping("/authorization")
-    public String showAuthorizationPage(){
+    public String showAuthorizationPage() {
         return "auth/authorization";
     }
 
     @GetMapping("/registration")
-    public String showRegistrationPage(){
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new UserDTO());
         return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String register(
+            @ModelAttribute("user") @Valid UserDTO userDTO,
+            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "auth/registration";
+        } else {
+            userService.registerNewUserAccount(userDTO);
+            return "redirect:home";
+        }
+
     }
 }
---
+++ src/main/java/me/vladislav/file_storage/services/UserService.java
@@ -0,0 +1,31 @@
+package me.vladislav.file_storage.services;
+
+import lombok.RequiredArgsConstructor;
+import me.vladislav.file_storage.UserAlreadyExistException;
+import me.vladislav.file_storage.dto.UserDTO;
+import me.vladislav.file_storage.models.User;
+import me.vladislav.file_storage.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+
+    public void registerNewUserAccount(UserDTO userDTO){
+        if(userRepository.findUserByLogin(userDTO.getLogin()).isPresent()){
+            throw new UserAlreadyExistException("A user with that login exists");
+        }
+
+//        TODO: encode password in UserService
+        User user = new User(
+                userDTO.getLogin(),
+//                passwordEncoder.encode(userDTO.getPassword())
+                userDTO.getPassword()
+        );
+
+        userRepository.save(user);
+    }
+}
