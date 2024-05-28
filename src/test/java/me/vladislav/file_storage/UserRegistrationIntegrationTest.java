b8ff2db5d11d50d15f8247d9524356e9cf8b4384
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-28 22:28:52 +0300

    feat: added UserRegistrationIntegrationTest.java (not the final version, corrections needed);

---
+++ src/test/java/me/vladislav/file_storage/UserRegistrationIntegrationTest.java
@@ -0,0 +1,82 @@
+package me.vladislav.file_storage;
+
+import me.vladislav.file_storage.models.User;
+import me.vladislav.file_storage.repositories.UserRepository;
+import me.vladislav.file_storage.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
+public class UserRegistrationIntegrationTest {
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer("postgres:14.11")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test");
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void beforeEach() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void shouldRegisterAndSaveNewUser() throws Exception {
+
+        String login = "maks_dubov";
+        String password = "123123123";
+
+
+        mockMvc.perform(post("/registration").param("login", login)
+                        .param("password", password))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+
+        User user = userRepository.findUserByLogin(login).orElse(null);
+        assertEquals(login, user.getLogin());
+        assertTrue(passwordEncoder.matches(password, user.getPassword()));
+
+    }
+
+    //TODO: will need more tests (min 2: check exception in User Service and check input validation)
+
+}
+
