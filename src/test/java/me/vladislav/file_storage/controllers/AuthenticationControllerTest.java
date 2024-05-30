359d1b709bbdcc8fceef02102ba5a7df5af8821a
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-30 19:57:24 +0300

    feat: added shouldReturnAnAuthorizationViewIfAnServletExceptionOccurs() method to AuthenticationControllerTest.java;

--- src/test/java/me/vladislav/file_storage/controllers/AuthenticationControllerTest.java
+++ src/test/java/me/vladislav/file_storage/controllers/AuthenticationControllerTest.java
@@ -1,19 +1,26 @@
 package me.vladislav.file_storage.controllers;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import me.vladislav.file_storage.dto.UserDTO;
 import me.vladislav.file_storage.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,10 +35,26 @@ public class AuthenticationControllerTest {
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private HttpServletRequest httpServletRequest;
+
+    @MockBean
+    private Model model;
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void shouldReturnRegistrationViewIfTheInputIsInvalid() throws Exception {
+    void shouldReturnARegistrationViewIfTheInputIsInvalid() throws Exception {
         String login = "1";
         String password = "1";
+
+        when(bindingResult.hasErrors()).thenReturn(true);
 
         mockMvc.perform(post("/registration")
                         .param("login", login)
@@ -40,5 +63,22 @@ public class AuthenticationControllerTest {
 
         assertNotEquals(null, bindingResult);
         verifyNoInteractions(userService);
+    }
+
+    @Test
+    void shouldReturnAnAuthorizationViewIfAnServletExceptionOccurs() throws ServletException {
+        String login = "maksim_nabokov";
+        String password = "123123123";
+        UserDTO userDTO = new UserDTO(login, password);
+
+        doThrow(new ServletException("Simulated ServletException")).when(httpServletRequest).login(anyString(), anyString());
+        when(bindingResult.hasErrors()).thenReturn(false);
+        doNothing().when(userService).registerNewUserAccount(any());
+
+        String viewName = authenticationController.register(userDTO, bindingResult, httpServletRequest, model);
+
+        assertEquals("auth/authorization", viewName);
+        verify(model).addAttribute("loginError", "Login failed. Please check your credentials.");
+        verify(httpServletRequest).login(login, password);
     }
 }
