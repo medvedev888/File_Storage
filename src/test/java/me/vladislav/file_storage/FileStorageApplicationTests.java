5ed801856cca00886016da3ba443c36bc4123208
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-01 23:35:41 +0300

    refactor: updated FileStorageApplicationTests.java;

--- src/test/java/me/vladislav/file_storage/FileStorageApplicationTests.java
+++ src/test/java/me/vladislav/file_storage/FileStorageApplicationTests.java
@@ -2,8 +2,10 @@ package me.vladislav.file_storage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class FileStorageApplicationTests {
 
     @Test
