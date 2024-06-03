80f5e4a4564b843c9286ca77c4ed5742ba737a51
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-03 22:11:23 +0300

    refactor: updated HomeController.java;

--- src/main/java/me/vladislav/file_storage/controllers/HomeController.java
+++ src/main/java/me/vladislav/file_storage/controllers/HomeController.java
@@ -1,12 +1,21 @@
 package me.vladislav.file_storage.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/")
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String showHomePage(){
+    @GetMapping
+    public String showHomePage(
+            @AuthenticationPrincipal User user,
+            Model model
+    ) {
+        model.addAttribute("login", user.getUsername());
         return "home";
     }
 }
