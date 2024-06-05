56969cf9da4458176c6c0fe3e685a213fbfde22d
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-05 21:20:54 +0300

    feat: added FolderCreationException.java;

---
+++ src/main/java/me/vladislav/file_storage/exceptions/folders/FolderCreationException.java
@@ -0,0 +1,11 @@
+package me.vladislav.file_storage.exceptions.folders;
+
+public class FolderCreationException extends RuntimeException {
+    public FolderCreationException(String message) {
+        super(message);
+    }
+
+    public FolderCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
