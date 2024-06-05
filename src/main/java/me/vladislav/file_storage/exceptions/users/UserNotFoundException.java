a7b8fdda4a01e9f375bfbdfc0020c4875632ceb2
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-05 21:19:36 +0300

    feat: added UserNotFoundException.java;

---
+++ src/main/java/me/vladislav/file_storage/exceptions/users/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package me.vladislav.file_storage.exceptions.users;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
