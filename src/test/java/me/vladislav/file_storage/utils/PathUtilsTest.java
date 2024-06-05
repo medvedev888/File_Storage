f42667e5c66500b90ecbce37c4bfd527c528de7e
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-05 22:29:40 +0300

    feat: added PathUtilsTest.java;

---
+++ src/test/java/me/vladislav/file_storage/utils/PathUtilsTest.java
@@ -0,0 +1,20 @@
+package me.vladislav.file_storage.utils;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PathUtilsTest {
+
+    @Test
+    void shouldReturnValidRootPathTest() {
+        String path = "/////haha//";
+        Long userID = 123L;
+
+        String resultPath = PathUtils.getRootPath(path, userID);
+
+        assertEquals("/user-123-files/haha/", resultPath);
+
+    }
+}
