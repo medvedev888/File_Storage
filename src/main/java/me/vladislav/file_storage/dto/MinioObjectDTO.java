986b43003d70d048e966fb0bd304a57ecfc888b0
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-07 18:33:41 +0300

    refactor: updated MinioObjectDTO.java;

--- src/main/java/me/vladislav/file_storage/dto/MinioObjectDTO.java
+++ src/main/java/me/vladislav/file_storage/dto/MinioObjectDTO.java
@@ -1,11 +1,14 @@
 package me.vladislav.file_storage.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MinioObjectDTO {
     private String path;
     private String name;
     private Boolean isFolder;
-    private String owner;
 }
