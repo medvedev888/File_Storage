5d2cb203b0c7db35568fe7847733eb20fca46596
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-07 18:34:23 +0300

    feat: added getFolders() to FolderService.java;

--- src/main/java/me/vladislav/file_storage/services/FolderService.java
+++ src/main/java/me/vladislav/file_storage/services/FolderService.java
@@ -1,13 +1,21 @@
 package me.vladislav.file_storage.services;
 
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.Result;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
+import me.vladislav.file_storage.dto.MinioObjectDTO;
 import me.vladislav.file_storage.exceptions.folders.FolderCreationException;
+import me.vladislav.file_storage.exceptions.folders.RetrievingFoldersException;
+import me.vladislav.file_storage.utils.PathUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +26,36 @@ public class FolderService {
     @Value("${spring.minio.client.bucket-name}")
     private String bucketName;
 
-    public void createFolder(String rootFolderPath, String folderName){
+    public void createFolder(String rootFolderPath, String folderName) {
         try {
             minioClient.putObject(PutObjectArgs.builder()
                     .bucket(bucketName)
                     .object(rootFolderPath + folderName + '/')
-                    .stream(new ByteArrayInputStream(new byte[] {}), 0, -1)
+                    .stream(new ByteArrayInputStream(new byte[]{}), 0, -1)
                     .build());
         } catch (Exception e) {
             throw new FolderCreationException("Error when creating folder.", e);
+        }
+    }
+
+    public List<MinioObjectDTO> getFolders(String path, boolean recursive) {
+        try {
+            List<MinioObjectDTO> listOfFolders = new ArrayList<>();
+
+            Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
+                    .bucket(bucketName)
+                    .prefix(path)
+                    .recursive(recursive)
+                    .build()
+            );
+
+            for (Result<Item> result : results) {
+                Item item = result.get();
+                listOfFolders.add(new MinioObjectDTO(path, PathUtils.getPathWithoutRootUserFolder(item.objectName()), item.objectName().endsWith("/")));
+            }
+            return listOfFolders;
+        } catch (Exception e) {
+            throw new RetrievingFoldersException("Error when creating folder.", e);
         }
     }
 }
