7c69ed2608b1e26fb33079db7b563fd027725006
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-07 18:32:51 +0300

    refactor: updated PathUtils.java;

--- src/main/java/me/vladislav/file_storage/utils/PathUtils.java
+++ src/main/java/me/vladislav/file_storage/utils/PathUtils.java
@@ -3,7 +3,25 @@ package me.vladislav.file_storage.utils;
 public class PathUtils {
 
     public static String getRootPath(String path, Long userID){
-        StringBuffer stringBuffer = new StringBuffer(getPathToRootUserFolder(userID) + path.strip());
+        return getValidPath(getPathToRootUserFolder(userID) + path);
+    }
+
+    public static String getPathWithoutRootUserFolder(String path) {
+        path = getValidPath(path);
+        for(int i = 0; i < path.length(); i++) {
+            if(path.charAt(i) == '/') {
+                return path.substring(i + 1);
+            }
+        }
+        return path;
+    }
+
+    public static String getPathToRootUserFolder(Long userID){
+        return "user-" + userID + "-files/";
+    }
+
+    public static String getValidPath(String path) {
+        StringBuffer stringBuffer = new StringBuffer(path.strip());
         for(int i = 0; i < stringBuffer.length() - 1; i++){
             if(stringBuffer.charAt(i) == '/' && stringBuffer.charAt(i + 1) == '/') {
                 stringBuffer.delete(i, i + 1);
@@ -11,10 +29,6 @@ public class PathUtils {
             }
         }
         return stringBuffer.toString().strip();
-    }
-
-    public static String getPathToRootUserFolder(Long userID){
-        return "/user-" + userID + "-files/";
     }
 
 }
