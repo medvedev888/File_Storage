640365158cf6a0d5c538b37c113d3feb57c1e2bc
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-07 18:35:29 +0300

    refactor: updated HomeController.java (not finished version);

--- src/main/java/me/vladislav/file_storage/controllers/HomeController.java
+++ src/main/java/me/vladislav/file_storage/controllers/HomeController.java
@@ -1,21 +1,42 @@
 package me.vladislav.file_storage.controllers;
 
+import lombok.RequiredArgsConstructor;
 import me.vladislav.file_storage.dto.FolderCreateDTO;
+import me.vladislav.file_storage.dto.MinioObjectDTO;
+import me.vladislav.file_storage.services.FolderService;
+import me.vladislav.file_storage.services.UserService;
+import me.vladislav.file_storage.utils.PathUtils;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequestMapping("/")
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final FolderService folderService;
+    private final UserService userService;
+
     @GetMapping
     public String showHomePage(
             @AuthenticationPrincipal User user,
+            @RequestParam(value = "path", required = false, defaultValue = "/") String path,
             Model model
     ) {
+
+        me.vladislav.file_storage.models.User currentUser = userService.getUserByLogin(user.getUsername());
+        path = PathUtils.getRootPath(path, currentUser.getId());
+
+        List<MinioObjectDTO> listOfFolders = folderService.getFolders(path, false);
+        model.addAttribute("listOfFolders", listOfFolders);
+
         model.addAttribute("folderCreateDTO", new FolderCreateDTO());
         return "home";
     }
