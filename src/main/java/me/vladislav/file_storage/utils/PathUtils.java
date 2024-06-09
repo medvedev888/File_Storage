780eedc4098e67adf1ace330083e2d4fdcd6e5d6
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-09 20:49:46 +0300

    refactor: updated PathUtils.java;

--- src/main/java/me/vladislav/file_storage/utils/PathUtils.java
+++ src/main/java/me/vladislav/file_storage/utils/PathUtils.java
@@ -2,28 +2,18 @@ package me.vladislav.file_storage.utils;
 
 public class PathUtils {
 
-    public static String getRootPath(String path, Long userID){
-        return getValidPath(getPathToRootUserFolder(userID) + path);
+    public static String getRootPath(String path, Long userID) {
+        return getValidPath(getPathWithRootUserFolder(userID) + path);
     }
 
-    public static String getPathWithoutRootUserFolder(String path) {
-        path = getValidPath(path);
-        for(int i = 0; i < path.length(); i++) {
-            if(path.charAt(i) == '/') {
-                return path.substring(i + 1);
-            }
-        }
-        return path;
-    }
-
-    public static String getPathToRootUserFolder(Long userID){
+    public static String getPathWithRootUserFolder(Long userID) {
         return "user-" + userID + "-files/";
     }
 
     public static String getValidPath(String path) {
         StringBuffer stringBuffer = new StringBuffer(path.strip());
-        for(int i = 0; i < stringBuffer.length() - 1; i++){
-            if(stringBuffer.charAt(i) == '/' && stringBuffer.charAt(i + 1) == '/') {
+        for (int i = 0; i < stringBuffer.length() - 1; i++) {
+            if (stringBuffer.charAt(i) == '/' && stringBuffer.charAt(i + 1) == '/') {
                 stringBuffer.delete(i, i + 1);
                 i--;
             }
