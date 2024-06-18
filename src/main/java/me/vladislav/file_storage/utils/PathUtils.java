7881408a04cb48c7d43e21bb06d2f82821d2cdd0
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-19 02:10:16 +0300

    refactor: updated PathUtils.java;
    added documentation for methods;

--- src/main/java/me/vladislav/file_storage/utils/PathUtils.java
+++ src/main/java/me/vladislav/file_storage/utils/PathUtils.java
@@ -1,8 +1,18 @@
 package me.vladislav.file_storage.utils;
 
-//TODO: разобраться с многопоточностью
 public class PathUtils {
 
+    /**
+     *
+     * @param path path containing {@code currentFolderName}
+     * @param currentFolderName name of current folder
+     * @param isNameRepeat a boolean flag indicating whether a path can have folders with the same name
+     * @param numberOfDesiredFolder position of desired folder, should be greater than zero, if {@code isNameRepeat=true}
+     * @return valid path (using {@link #getValidPath(String path)}) without {@code currentFolderName}; empty string,
+     * if path is empty
+     * @throws IllegalArgumentException
+     *
+     */
     public static String getPathWithoutCurrentFolder(String path, String currentFolderName, boolean isNameRepeat, int numberOfDesiredFolder) {
         path = getValidPath(path);
         if (path.isEmpty()) {
@@ -29,10 +39,20 @@ public class PathUtils {
         throw new IllegalArgumentException("Invalid input.");
     }
 
+    /**
+     *
+     * @param path any path
+     * @param userID id of user
+     * @return valid path (using {@link #getValidPath(String path)}) with prefix (user-{@code userID}-files)
+     */
     public static String getRootPath(String path, Long userID) {
         return getValidPath(getPathWithRootUserFolder(userID) + path);
     }
 
+    /**
+     * @param path path containing root user folder
+     * @return valid path (using {@link #getValidPath(String path)}) without prefix (user-{@code userID}-files)
+     */
     public static String getPathWithoutRootUserFolder(String path) {
         path = getValidPath(path);
         for (int i = 0; i < path.length(); i++) {
@@ -43,10 +63,19 @@ public class PathUtils {
         return path;
     }
 
+    /**
+     * @param userID id of user
+     * @return string of type (user-{@code userID}-files)
+     */
     public static String getPathWithRootUserFolder(Long userID) {
         return "user-" + userID + "-files/";
     }
 
+    /**
+     *
+     * @param path any path
+     * @return valid path (without whitespace characters and duplicate characters {@code /})
+     */
     public static String getValidPath(String path) {
         StringBuffer stringBuffer = new StringBuffer(path.strip());
         for (int i = 0; i < stringBuffer.length() - 1; i++) {
