cc525766c7fbff564f41334964a4cc62accc41a0
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-19 02:12:21 +0300

    feat: added FolderDeleteDTO.java;

---
+++ src/main/java/me/vladislav/file_storage/dto/folder/FolderDeleteDTO.java
@@ -0,0 +1,20 @@
+package me.vladislav.file_storage.dto.folder;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FolderDeleteDTO {
+
+    private String rootFolderPath;
+
+    @Size(min = 1, max = 50, message="Not valid size of folder name.")
+    @Pattern(regexp = "^[a-zA-Z0-9._/ -]*$", message = "Name of folder must contain only alphanumeric characters and \'.\', \'-\', \'_\', \' \'.")
+    private String folderName;
+
+}
