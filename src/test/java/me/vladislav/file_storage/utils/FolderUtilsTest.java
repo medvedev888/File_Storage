a0248fe22db16b61e1096777cc5a27e0fa262769
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-21 19:24:22 +0300

    refactor: updated FolderUtilsTest.java;

--- src/test/java/me/vladislav/file_storage/utils/FolderUtilsTest.java
+++ src/test/java/me/vladislav/file_storage/utils/FolderUtilsTest.java
@@ -14,9 +14,9 @@ public class FolderUtilsTest {
         String name2 = "folder";
         String name3 = "haha/";
 
-        String result1 = FolderUtils.getOwnerFolder(path1, name1, true, 1);
-        String result2 = FolderUtils.getOwnerFolder(path2, name2, false, 123);
-        String result3 = FolderUtils.getOwnerFolder(path2, name3, false, 123);
+        String result1 = FolderUtils.getOwnerFolder(path1, name1, true, 1L);
+        String result2 = FolderUtils.getOwnerFolder(path2, name2, false, 123L);
+        String result3 = FolderUtils.getOwnerFolder(path2, name3, false, 123L);
 
         assertEquals("user-1-files/", result1);
         assertEquals("user-1-files/", result2);
@@ -30,5 +30,14 @@ public class FolderUtilsTest {
         String result = FolderUtils.getNameOfCurrentFolderByPath(path);
 
         assertEquals("folder-2", result);
+    }
+
+    @Test
+    void getTheNumberOfTheDuplicateFolderNameByPath() {
+        String path = "user-1-files/123/11/11/12/11/";
+
+        Long result = FolderUtils.getTheNumberOfTheDuplicateFolderNameByPath(path);
+
+        assertEquals(3, result);
     }
 }
