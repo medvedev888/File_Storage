748c46dd4e80139f4d28b1ed28978b5cf53ff945
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-23 19:46:00 +0300

    feat: added FileUploadException.java;

---
+++ src/main/java/me/vladislav/file_storage/exceptions/file/FileUploadException.java
@@ -0,0 +1,11 @@
+package me.vladislav.file_storage.exceptions.file;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
