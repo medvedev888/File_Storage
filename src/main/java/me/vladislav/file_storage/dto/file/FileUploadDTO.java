c9b840d15649e85d2a11a47195c2834e0459870c
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-23 18:37:39 +0300

    refactor: updated FileUploadDTO.java;

--- src/main/java/me/vladislav/file_storage/dto/file/FileUploadDTO.java
+++ src/main/java/me/vladislav/file_storage/dto/file/FileUploadDTO.java
@@ -1,2 +1,17 @@
-package me.vladislav.file_storage.dto.file;public class FileUploadDTO {
+package me.vladislav.file_storage.dto.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileUploadDTO {
+
+    //TODO: need to add validation
+    private MultipartFile file;
+    private String rootFolderPath;
+
 }
