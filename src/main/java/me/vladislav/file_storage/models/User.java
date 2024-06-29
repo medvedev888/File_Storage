f0d49280a0e7ae2c3e7e3b0f70a8d3745900ab0b
vladislavmedvedev <EnoT3581@mail.ru>
2024-06-29 17:18:55 +0300

    refactor: updated User.java;

--- src/main/java/me/vladislav/file_storage/models/User.java
+++ src/main/java/me/vladislav/file_storage/models/User.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 })
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
